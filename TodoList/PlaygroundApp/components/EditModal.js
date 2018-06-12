3d111419109599444078c8c651948bf721c92337
chenling <deanli72@gmail.com>
2018-06-12 22:48:10 +0800

    add todos folder

---
+++ TodoList/PlaygroundApp/components/EditModal.js
@@ -0,0 +1,105 @@
+import Todos from '../model/Todos';
+import React from 'react';
+import { Dimensions, StyleSheet, Text, View, TextInput, Alert } from 'react-native';
+import Modal from 'react-native-modalbox';
+import Button from 'react-native-button';
+import PropTypes from 'prop-types';
+
+var screen = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  modal: {
+    height: screen.height / 2,
+    width: screen.width - 60,
+    justifyContent: 'center',
+    borderRadius: 30,
+    shadowRadius: 10,
+    backgroundColor: 'white',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  inputText: {
+    height: 40,
+    textAlign: 'center',
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+    fontSize: 18,
+    margin: 20,
+  },
+  inputDescText: {
+    height: 40,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+    fontSize: 18,
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  saveBotton: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  saveBottonContainer: {
+    padding: 8,
+    marginLeft: 60,
+    marginRight: 60,
+    height: 40,
+    backgroundColor: 'gold',
+    borderRadius: 6,
+    marginTop: 30,
+  }
+});
+
+export default class EditModal extends React.Component {
+  static propTypes = {
+    editTodo: PropTypes.func.isRequired,
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      index: null,
+      todoDescription: '',
+    };
+  }
+  showEditModal = (todo, index) => {
+    this.setState({
+      index: index,
+      name: todo.name,
+      todoDescription: todo.description,
+    });
+    this.refs.myEditModal.open();
+  };
+  render() {
+    return (
+      <Modal ref={'myEditModal'} style={styles.modal} position='center'>
+        <Text style={styles.title}>Edit Todo</Text>
+        <TextInput style={styles.inputText}
+          onChangeText={(text) => this.setState({ name: text })}
+          placeholder="Edit todo"
+          value={this.state.name} />
+        <TextInput style={styles.inputDescText}
+          onChangeText={(text) => this.setState({ todoDescription: text })}
+          placeholder="Description"
+          value={this.state.todoDescription} />
+        <Button style={styles.saveBotton}
+          containerStyle={styles.saveBottonContainer}
+          onPress={() => {
+            if (this.state.name.length == 0 || this.state.todoDescription.length == 0) {
+              Alert.alert('You must enter todo and description');
+              return;
+            }
+            Todos[this.state.index].name = this.state.name;
+            Todos[this.state.index].description = this.state.todoDescription;
+            this.props.editTodo(Todos[this.state.index]);
+            this.refs.myEditModal.close();
+            this.setState({ name: '' });
+          }}>Save</Button>
+      </Modal>
+    );
+  }
+}
---
+++ TodoList/PlaygroundApp/components/TodoItem.js
@@ -0,0 +1,74 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { StyleSheet, Text, View, Alert, TouchableOpacity } from 'react-native';
+import Swipeout from 'react-native-swipeout';
+
+const styles = StyleSheet.create({
+  itemText: {
+    padding: 12,
+    fontSize: 20,
+    height: 50,
+  }
+});
+
+
+export default class TodoItem extends React.Component {
+  static propTypes = {
+    item: PropTypes.object.isRequired,
+    index: PropTypes.number,
+    onPressDelete: PropTypes.func,
+    onPressEdit: PropTypes.func
+  };
+
+  onEditTodo = () => {
+    this.props.onPressEdit(this.props.item, this.props.index);
+  };
+
+  deleteTodo = () => {
+    this.props.onPressDelete(this.props.index);
+  };
+
+  render() {
+    var swipeoutBtns = [
+      {
+        text: 'Edit', type: 'primary',
+        onPress: () => {
+          this.onEditTodo();
+        }
+      },
+      {
+        text: 'Delete',
+        backgroundColor: 'red',
+        type: 'delete',
+        onPress: () => {
+          Alert.alert(
+            'Alert',
+            'Are you sure you want to delete?',
+            [
+              { text: 'No', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+              { text: 'Yes', onPress: () => { this.deleteTodo() } },
+            ],
+            { cancelable: true }
+          );
+        },
+      }
+    ]
+    return (
+      <Swipeout
+        backgroundColor="transparent"
+        right={swipeoutBtns}
+        autoClose={true} >
+
+        <TouchableOpacity
+          style={{ flex: 1, backgroundColor: this.props.index % 2 == 0 ? 'transparent' : '#dddcdcdc' }}
+          onPress={() => {
+            Alert.alert(this.props.item.description);
+          }
+          }>
+          <Text style={styles.itemText}>{this.props.item.name}</Text>
+        </TouchableOpacity>
+      </Swipeout>
+
+    );
+  }
+}
